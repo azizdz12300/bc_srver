022697beb12b0b700d0ef2851a9780f9d861a259
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-03-26 14:13:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,42 @@
-﻿          client.on('message', message => {
-            var prefix = "+"; //walter.js#2002
-                  if(!message.channel.guild) return;
-        if(message.content.startsWith(prefix + 'bc')) {
-        if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط ل السيرفرات**').then(m => m.delete(5000));
-      if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**`ADMINISTRATOR`أنت لا تملك خاصية **' );
-        let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-        let copy = "ITZ Bot";
-        let request = `Requested By ${message.author.username}`;
-        if (!args) return message.reply('**اكتب رسالة البرودكاست**');message.channel.send(`**هل أنت متأكد من ارسال البرودكاست؟ \nالبرودكاست: ** \` ${args}\``).then(msg => { //walter.js#2002
-        msg.react('✅')
-        .then(() => msg.react('❌'))
-        .then(() =>msg.react('✅'))
-        
-        let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-        let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-        
-        let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-        let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-     reaction1.on("collect", r => {
-        message.channel.send(`**☑ | عضو  __${message.guild.members.size}__ تم ارسال البرودكاست ل **`).then(m => m.delete(5000));
-        message.guild.members.forEach(m => {
-      
-      var bc = new
-           Discord.RichEmbed()
-           .setColor('RANDOM')
-           .setTitle('Broadcast')
-           .addField('🔰السيرفر🔰', message.guild.name) //walter.js#2002
-           .addField('🚩صاحب الرسالة🚩', message.author.username) //walter.js#2002
-           .addField('📜الرسالة📜', args)  //walter.js#2002
-           .setThumbnail(message.author.avatarURL)
-           .setFooter(copy, client.user.avatarURL);
-        m.send({ embed: bc })
-        msg.delete();
-        })
-        })
-        reaction2.on("collect", r => {
-        message.channel.send(`**تم الغاء البرودكاست :x:**`).then(m => m.delete(5000));
-        msg.delete();
-        })
-        })
-        }
-        }); //walter.js#2002
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const prefix = "-";
+var usage = `\`${prefix}bc <content>\``
+
+
+client.on("message", (message) => {
+  if (message.author.bot) return;
+  if (message.content.indexOf(prefix) != 0) return;
+  const [command, ...args] = message.content.slice(prefix.length).split(/ +/g);
+  if (command === "bc") {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`**?|${message.author.username}, Admins Only`);
+    if (args.length == 0) message.channel.send(`**?|${message.author.username}, the correct usage is ${usage}**`);
+    var idx = 0, fails = 0;
+    message.channel.send(`${idx} received from ${message.guild.members.size} \n ${fails} fails`).then(res => {
+    setInterval(() => {
+      res.edit(`${idx} received from ${message.guild.members.size} \n ${fails} fails`)
+    }, 3000)
+      message.guild.members.map(member => {
+        var content = args.join(" ").replace(/\[mention\]|\[ping\]|\[user\]/g, `<@${member.id}>`) // use less variables in loop
+        setTimeout(() => {
+          member.send({
+            embed: {
+              description: content,
+              color: 990000,
+              footer: {
+                text: message.guild.name
+              }
+            }
+          }).then(() => {
+            idx++;
+          }).catch((err) => {
+            fails++;
+          });
+        }, idx * 5);
+      });
+    });
+  }
+});
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
